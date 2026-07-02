85a049c0172ef0f0135dd0f67175b33237258e10
Dom1n1kSk <Hulikdominik@gmail.com>
2026-07-02 12:00:54 +0200

    Add files via upload

---
+++ druhy_program.py
@@ -0,0 +1,11 @@
+vek = int(input("Koľko máš rokov? "))
+print(vek)
+
+if vek >= 18:
+    print("Si plnoletý")
+else:
+    print("Nie si neplnoletý")
+
+
+print("Ďakujem za použitie programu")
+
---
+++ prvy_program.py
@@ -0,0 +1,8 @@
+meno = input("Ako sa voláš? ")
+vek = input("Koľko máš rokov? ")
+
+print("Ahoj")
+print(meno)
+print("Máš")
+print(vek)
+print("rokov")
